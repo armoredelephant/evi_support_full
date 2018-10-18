ec6d381932a6a9073f49050dee4eea563a3a4992
Keith Alleman <keith.alleman@icloud.com>
2018-10-17 21:03:45 -0600

    project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'reacrt-dom';
+import ReactDOM from 'react-dom';
 
 // Without resolve: in webpack.config, we would need to use ./App.jsx
 import App from './App';
+import './sass/base.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
