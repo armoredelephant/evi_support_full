4b30b0ff4e1f15a14d147518ee2b689a791a7aab
Keith Alleman <keith.alleman@icloud.com>
2018-10-18 05:58:01 -0600

    Changes to import from LogoLink comonent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import LogoLink from './LogoLink';
 
 class Navbar extends React.Component {
     render() {
         return (
-            <div>This is my navbar</div>
+            <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+                <LogoLink />
+            </nav>
         )
     }
 }
