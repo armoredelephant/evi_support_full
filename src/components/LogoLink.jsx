c0bc6e8e2ee360830e9fca196fc48df56731d31b
Keith Alleman <keith.alleman@icloud.com>
2018-10-25 12:05:57 -0600

    Changed my mind about the design and remade all of the css and classes

--- src/components/LogoLink.jsx
+++ src/components/LogoLink.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 class EviLogo extends React.Component {
     render() {
         return (
-            <div className="header-logo">
-                <a href="#" className="navbar-brand" id="evi-logo">
-                    <img src="/resources/images/logo.png" alt="logo" id="evi-logo" />
-                </a>
-                <h1 className="page-title">Self-Help</h1>
+            <div className="mobile-header">
+                <div className="mobile-header-inner">
+                    <a href="#" className="header-link" id="evi-logo">
+                        <img src="/resources/images/logo.png" alt="logo" className="header-logo" />
+                    </a>
+                </div>
             </div>
         );
     }
--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SideNavBar extends React.Component {
     render() {
         return (
-            console.log("do nothing atm")
+            null
         );
     }
 }
