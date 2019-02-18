f11c50f0ea01be126aa5dd957563b3912d2fdf5b
Keith Alleman <keith.alleman@icloud.com>
2019-02-18 14:55:01 -0700

    working on getting the edit article to post and replace old article. Delete step is now working properly on the edit page. current selected article to edit's index is being stored in the state now which will be used to replace the old article at that index in the DB.

--- src/components/Admin/PostArticle/DashboardFormSteps.jsx
+++ src/components/Admin/PostArticle/DashboardFormSteps.jsx
@@ -2,38 +2,45 @@ import React from 'react';
 import ImageButtonDisplay from '../../elements/ImageButtonDisplay';
 
 const DashboardFormSteps = ( props ) => {
-    console.log(props.uploadComplete)
     return (
         <ul className="steps-list">
             {props.steps.map((field, index) => {
                 return (
                     <li key={index} >
-                    <div>
-                        <input 
-                            onChange={props.handleStepInput}
-                            name={index}
-                            type="text" 
-                            value={field.step} />
-                    </div>
-                    <div className="upload-image-div">
-                        <input 
-                            accept="image/png image/jpeg"
-                            className="file-input" 
-                            id={`file-input-${index}`} 
-                            name={index} 
-                            onChange={props.handleImage}
-                            src="#" 
-                            type="file" 
-                            value={field.imgUrl} />
-                        <label className="image-upload-label" 
-                            htmlFor={`file-input-${index}`}
-                            id="image-upload-label">
-                        <ImageButtonDisplay 
-                            uploadComplete={props.uploadComplete}
-                            isLoading={props.isLoading}
-                            imgName={field.imgName} />
-                        </label>
-                    </div>
+                        <div>
+                            <input 
+                                onChange={props.handleStepInput}
+                                name={index}
+                                type="text" 
+                                value={field.step} />
+                        </div>
+                        <div className="image-upload-wrapper">
+                            <div className="upload-image-div">
+                                <input 
+                                    accept="image/png image/jpeg"
+                                    className="file-input" 
+                                    id={`file-input-${index}`} 
+                                    name={index} 
+                                    onChange={props.handleImage}
+                                    src="#" 
+                                    type="file" 
+                                    value={field.imgUrl} />
+                                <label className="image-upload-label" 
+                                    htmlFor={`file-input-${index}`}
+                                    id="image-upload-label">
+                                <ImageButtonDisplay 
+                                    uploadComplete={props.uploadComplete}
+                                    isLoading={props.isLoading}
+                                    imgName={field.imgName} />
+                                
+                                </label>
+                            </div>
+                            <div className="upload-image-div-remove">
+                                    {props.adminAction === 'edit' && props.steps.length > 1
+                                    ? <button name={`step-${index}`} className='image-remove-button' onClick={props.handleStepDelete}>Remove step</button>
+                                    : null }
+                            </div>
+                        </div>
                     </li>
                 )
             })}
--- src/components/Admin/PostArticle/EditArticle.jsx
+++ src/components/Admin/PostArticle/EditArticle.jsx
@@ -16,6 +16,7 @@ class EditArticle extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            articleIndex: 0,
             articleChosen: false,
             articleList: [],
             category: 'choose a category',
@@ -50,6 +51,7 @@ class EditArticle extends Component {
             axios.get(`${API_HOST_URL}/api/articles/single-article`, options)
                 .then(response => {
                     this.setState({
+                        articleIndex: response.data.articleIndex,
                         selectedArticle: response.data.article,
                         updatedTagsList: response.data.article.tags,
                         updatedSteps: response.data.article.body // [0] if routing the whole article
@@ -82,42 +84,91 @@ class EditArticle extends Component {
         this.setState({ category: target.value })
     }
 
-    handleImage = ( event, files ) => {
+    handleImage = ( event ) => {
         const target = event.target
-        const { updsatedSteps } = this.state;
+        const files = target.files
+        const { updatedSteps } = this.state;
 
         let oldSteps = Array.from(updatedSteps);
         oldSteps[target.name].imgName = files[0].name
-        oldSteps[target.name].imgData = files[0] 
-        
+        oldSteps[target.name].stepIndex = target.name
+        oldSteps[target.name].file = files[0]
+
         const newSteps = oldSteps
 
         this.setState({
             updatedSteps: newSteps
-        })
+        });
     }
 
     handleInputChange = ( event ) => {
         const target = event.target;
         const value = target.value;
         const name = target.name;
+
+        const oldState = this.state.selectedArticle
+        oldState[name] = value
         
         this.setState({
-            selectedArticle: { [name]: value }
+            selectedArticle: oldState
         });
 
     }
 
     handleStepAdd = ( event ) => {
         event.preventDefault();
-        const { updatedSteps } = this.state
+        const { updatedSteps, selectedArticle } = this.state
         const arrayOfSteps = Array.from(updatedSteps)
-        const nextStep = { step: '', imgName: null, imgData: '' }
+        const nextStep = { step: '', imgName: null, imgData: '', file: null }
         const newSteps = [...arrayOfSteps, nextStep];
 
+        const oldState = selectedArticle
+        oldState.body = newSteps
+
         this.setState({
+            selectedArticle: oldState,
             updatedSteps: newSteps
         })
+    }
+
+    handleStepDelete = ( event ) => {
+        event.preventDefault();
+        const target = event.target;
+        const indexToDelete = Number(target.name.split('-')[1]);
+        const { updatedSteps, selectedArticle } = this.state
+        const arrayOfSteps = selectedArticle.body
+        console.log(arrayOfSteps)
+        if (arrayOfSteps.length === 1) {
+            const newSteps = [{ step: '', imgName: null, imgData: '', file: null }]
+            const oldState = selectedArticle
+            oldState.body = newSteps
+
+            this.setState({
+                selectedArticle: oldState,
+                updatedSteps: newSteps
+            })
+        } else {
+            const newSteps = arrayOfSteps
+            newSteps.splice(indexToDelete, 1)
+
+            const oldState = selectedArticle;
+            oldState.body = newSteps
+
+            this.setState({
+                selectedArticle: oldState,
+                updatedSteps: newSteps
+            })
+        }
+        // const filteredSteps = arrayOfSteps.splice(indexToDelete, 1)
+        // console.log(filteredSteps)
+
+        // const oldState = selectedArticle
+        // oldState.body = filteredSteps
+
+        // this.setState({
+        //     selectedArticle: oldState,
+        //     updatedSteps: filteredSteps
+        // })
     }
 
     handleStepInput = ( event ) => {
@@ -142,10 +193,13 @@ class EditArticle extends Component {
         const { updatedTagsList } = this.state
         const newTagArray = updatedTagsList.filter(tag => { return tag !== target.value })
 
+        const oldState = this.state.selectedArticle
+        oldState.tags = newTagArray
+        
         this.setState({
+            selectedArticle: oldState,
             updatedTagsList: newTagArray
-        })
-
+        });
     }
 
     handleTagInput = ( event ) => {
@@ -173,10 +227,87 @@ class EditArticle extends Component {
 
         const newTagsArray = prevTagsArray.concat(splitTags)
 
+        const oldState = this.state.selectedArticle
+        oldState.tags = newTagsArray
+        
         this.setState({
             tagInput: '',
+            selectedArticle: oldState,
             updatedTagsList: newTagsArray
-        })
+        });
+    }
+
+    // need to update the options to post. Updated article is stored in selectedArticle here. Or remake how the title/description is stored.
+    // steps will need to be renamed to selectedArticle.steps.map()
+    // Can remove the axios.get altogether as we already have the article
+    // Index that the article is to be "set" at is in this.state.articleIndex
+    handleSubmit = () => {
+        const { category } = this.state;
+        const options = {
+            params: {
+                category: category
+            }
+        }
+
+        axios.get(`${API_HOST_URL}/api/dashboard/`, options)
+            .then(response => {
+                const { category, description, steps, tags, title } = this.state;
+
+                const options = {
+                    category: category,
+                    categoryItemIndex: nextAvailableIndex,
+                    description: description,
+                    itemId: itemIdIncrement,
+                    steps: steps,
+                    tags: tags,
+                    title: title
+                }
+                if (category !== 'choose a category') {
+                    this.setState({ isPosting: true }, () =>{
+                        axios.post(`${API_HOST_URL}/api/dashboard/post-article`, options)
+                        .then(response => {
+                            steps.map(step => {
+                                if (step.file !== null) {
+                                    const data = new FormData();
+                                    data.append('file', step.file, step.file.name)
+                                    data.append('title', title)
+                                    data.append('index', step.stepIndex)
+                            
+                                    const options = {
+                                        data,
+                                        method: 'POST',
+                                        config: {
+                                            headers: {
+                                                'Content-Type': 'multipart/form-data'
+                                            }
+                                        },
+                                        url: `${API_HOST_URL}/api/dashboard/post-image`
+                                    }
+                                    axios(options)
+                                }
+                                if (response.data.message === 'success') {
+                                    this.setState({
+                                        category: 'choose a category',
+                                        description: '',
+                                        isPosting: false, 
+                                        postMessage: 'Article has successfully posted!',
+                                        steps: [{step: '', imgName: null, stepIndex: '', file: null}],
+                                        title: '',
+                                        tagInput: '',
+                                        tags: [],
+                                        uploadComplete: true
+                                    }) 
+                                }
+                            })
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        });
+                    })
+                } else {
+                    alert('Please select a valid category.')
+                }
+            })
     }
 
     // Can be refactored to have switch statements to determine which props get passed down.
@@ -219,11 +350,22 @@ class EditArticle extends Component {
                                 stepAction={stepAction} />
                             {selectedArticle.body.length !== 0 
                             ? <DashboardFormSteps  
+                                adminAction={adminAction}
                                 handleImage={this.handleImage}
+                                handleStepDelete={this.handleStepDelete}
                                 handleStepInput={this.handleStepInput}
+                                handleStepAdd={this.handleStepAdd}
                                 steps={updatedSteps} />
                             : null
                             }
+                             <div className="submit-wrapper">
+                                <button type="button" className="form-button submit-button" onClick={this.handleSubmit}>
+                                Submit
+                                </button>
+                            </div>
+                            <div className="post-message">
+                            {this.state.postMessage}
+                            </div>
                         </Fragment>
                         : null }
                     </Fragment>
